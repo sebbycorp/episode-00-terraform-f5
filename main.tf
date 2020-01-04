bfe92e0d4f6caa034cf017c2e426d2dd90e81a44
Sebastian Maniak <sebby@Sebastians-MacBook-Pro.local>
2020-01-04 10:25:47 -0500

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "bigip_do"  "do-example" {
-     do_json = "${file("example.json")}"
-     config_name = "sample_test1"
+resource "bigip_do"  "do-f5" {
+     do_json = "${files("do.json")}"
+     config_name = "deploy-do-f5"
+ }
+
+ resource "bigip_as3"  "as3-app1" {
+     as3_json = "${files("app1.json")}"
+     config_name = "config-app1"
  }
