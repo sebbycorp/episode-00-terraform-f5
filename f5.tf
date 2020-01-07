77b849163e9bace932862069c9ee73d96df48fb1
Sebastian Maniak <sebastian@maniak.io>
2020-01-06 19:49:28 -0500

    update

--- f5.tf
+++ f5.tf
@@ -1,6 +1,6 @@
 provider "bigip" {
     version = "~> 1.1"
-    address = "192.168.1.100"
+    address = "192.168.1.121"
     username = "admin"
     password = "W3lcome098!"
 }
--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "bigip_do"  "do-f5" {
-     do_json = "${files("do.json")}"
+     do_json = "${file("/files/do.json")}"
      config_name = "deploy-do-f5"
  }
 
  resource "bigip_as3"  "as3-app1" {
-     as3_json = "${files("app1.json")}"
+     as3_json = "${file("/files/app1.json")}"
      config_name = "config-app1"
  }
