4c2ec5a55e6f562bcaf10ffaa477a1f45dd49b1d
Sebastian Maniak <sebastian@maniak.io>
2020-01-06 20:00:48 -0500

    update

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "bigip_do"  "do-f5" {
      as3_json = "${file("/files/app1.json")}"
      config_name = "config-app1"
  }
+ 
+ resource "bigip_ltm_node" "node" {
+  name             = "/Common/terraform_node1"
+  address          = "172.16.3.103"
+  connection_limit = "0"
+  dynamic_ratio    = "1"
+  monitor          = "/Common/icmp"
+  description      = "Test-Node"
+  rate_limit       = "disabled"
+}
