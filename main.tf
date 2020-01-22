91a43c366304f8419dc4ec7d4f694a0e38147ad3
Sebastian Maniak <sebby@Sebastians-MacBook-Pro.local>
2020-01-22 17:08:59 -0500

    update

--- main.tf
+++ main.tf
@@ -3,19 +3,18 @@ resource "bigip_do" "do-f5" {
   config_name = "deploy-do-f5"
 }
 
-resource "bigip_as3" "as3-app1" {
-  as3_json    = "${file("apps/as3-app1.json")}"
-  config_name = "as3"
-}
-
 # Loading from a file is the preferred method
 resource "bigip_ltm_irule" "rule" {
   name  = "/Common/redirect.irule"
   irule = "${file("irules/redirect.tcl")}"
 }
 
-#Application Modules
+resource "bigip_as3" "as3-app1" {
+  as3_json    = "${file("apps/as3-app1.json")}"
+  config_name = "as3"
+}
 
+#Application Modules
 module "apps" {
   source = "./apps"
 
