5a76325624920bb30c9c76109207c85f6d276b6d
Sebastian Maniak <sebby@Sebastians-MacBook-Pro.local>
2020-01-22 06:24:33 -0500

    update

--- apps/app1.tf
+++ apps/app1.tf
@@ -1,14 +1,14 @@
 #
-resource "bigip_ltm_monitor" "terraform_monitor" {
-  name        = "/Common/terraform_monitor"
+resource "bigip_ltm_monitor" "terraform-monitor" {
+  name        = "/Common/terraform-monitor"
   parent      = "/Common/http"
   send        = "GET /some/path\r\n"
   timeout     = "999"
   interval    = "999"
 }
 
-resource "bigip_ltm_node" "node_nginx_server_01" {
-  name             = "/Common/nginx_server_01"
+resource "bigip_ltm_node" "node-nginx-server-01" {
+  name             = "/Common/nginx-server-01"
   address          = "10.20.0.5"
   connection_limit = "0"
   dynamic_ratio    = "1"
@@ -16,3 +16,4 @@ resource "bigip_ltm_node" "node_nginx_server_01" {
   description      = "Test-Node"
   rate_limit       = "disabled"
 }
+
--- apps/app2.tf
+++ apps/app2.tf
@@ -1,15 +1,15 @@
 #The following application builds
 
-resource "bigip_ltm_monitor" "website_monitor" {
-  name        = "/Common/website_monitor"
+resource "bigip_ltm_monitor" "website-monitor" {
+  name        = "/Common/website-monitor"
   parent      = "/Common/http"
   send        = "GET /some/path\r\n"
   timeout     = "999"
   interval    = "999"
 }
 
-resource "bigip_ltm_node" "node_nginx_server_02" {
-  name             = "/Common/nginx_server_02"
+resource "bigip_ltm_node" "node_nginx-server-02" {
+  name             = "/Common/nginx-server-02"
   address          = "10.20.0.6"
   connection_limit = "0"
   dynamic_ratio    = "1"
